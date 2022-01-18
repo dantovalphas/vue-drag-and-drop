3db0f31fe897b84cfccdd464594adca5c64e3a6f
Daniel Tovar <danieldaniyyelda@gmail.com>
2022-01-18 14:47:07 -0400

    fix code smell and up in other branch
    Signed-off-by: Daniel Tovar <danieldaniyyelda@gmail.com>

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -311,7 +311,8 @@ export default {
           //correct list, now find card
           for (let card of list.cards) {
             if (card.id === that.currentCard.id) {
-              card = that.currentCard;
+              //card = that.currentCard;
+              console.log("card", card);
             }
           }
         }
